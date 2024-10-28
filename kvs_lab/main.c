03c76dc20b093eb4e3b138eff2b73d7ab796bbba
hojjoong <163098674+hojjoong@users.noreply.github.com>
2024-10-29 00:40:30 +0900

    Add files via upload

---
+++ kvs_lab/main.c
@@ -0,0 +1,23 @@
+#include "kvs.h"
+#include "math.h"  //테스트중
+int main()
+{
+	kvs_t* kvs = open();
+
+	if(!kvs) {
+		printf("Failed to open kvs\n");
+		return -1;
+	}
+
+	// workload execution  
+	
+	// 1) 	file read 
+	// 2) 	if put, insert (key, value) into kvs.
+	// 		if get, seach the key in kvs and return the value. 
+	//		Return -1 if the key is not found  
+
+
+	close(kvs);
+	
+	return 0;
+}
